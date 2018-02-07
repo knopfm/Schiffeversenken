8137507d8e756e655006e140b5ea25de58eab485
MacRoy21 <philong246@gmx.de>
2018-02-07 16:00:31 +0100

    a

--- Schiffeversenken/Form1.vb
+++ Schiffeversenken/Form1.vb
@@ -1,8 +1,7 @@
 ﻿Public Class Form1
     Dim WithEvents NetzwerkClient As Krypter.KrypterInterface
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("Hi")
-    End Sub
+
 End Class
 'TODO: Schiffeversenken Programmieren
+'hallo
